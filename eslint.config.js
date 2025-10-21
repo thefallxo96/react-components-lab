026e18e81e55b9c23595476776dbd490d95cc8f8
thefallxo96 <titodelvalle824@gmail.com>
2025-10-21 19:20:12 -0400

    init commit

--- eslint.config.js
+++ eslint.config.js
@@ -24,8 +24,6 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-       'react/prop-types': 'off', // add this line
-      'react/no-unescaped-entities': 'off', // add this line
     },
   },
 ])
